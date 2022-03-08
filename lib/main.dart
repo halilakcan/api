452dbf607140c80e24daefb69486d23156e3e0fb
halilakcan <30620539+halilakcan@users.noreply.github.com>
2022-03-08 17:03:21 +0100

    for exchange

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,14 @@ Future<Album> fetchAlbum() async {
   return Album.fromJson(jsonDecode(response.body));
 }
 
-Future<Album> updateAlbum(double text1,double text2,double text3, double text4,double text5) async {
+getraterub() async {
+  final response = await http.get(Uri.parse(
+      'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=EUR&to_currency=RUB&apikey=7UK1NW1UZ8NZQT07'));
+  return jsonDecode(response.body);
+}
+
+Future<Album> updateAlbum(double text1, double text2, double text3,
+    double text4, double text5) async {
   final response = await http.patch(
     Uri.parse(
         'https://angular-argon-331323-default-rtdb.firebaseio.com/house.json'),
@@ -19,15 +26,15 @@ Future<Album> updateAlbum(double text1,double text2,double text3, double text4,d
       'Content-Type': 'application/json; charset=UTF-8',
     },
     body: jsonEncode(<String, double>{
-    "PLN": text1,
-    "EUR": text2,
-    "RUB": text3,
-    "CZK": text4,
-    "KZT": text5
+      "PLN": text1,
+      "EUR": text2,
+      "RUB": text3,
+      "CZK": text4,
+      "KZT": text5
     }),
   );
-    return Album.fromJson(jsonDecode(response.body));
-  }
+  return Album.fromJson(jsonDecode(response.body));
+}
 
 void main() => runApp(const MyApp());
 
@@ -45,12 +52,15 @@ class _MyAppState extends State {
   final TextEditingController controller4 = TextEditingController();
   final TextEditingController controller5 = TextEditingController();
   late Future<Album> futureAlbum;
-  var text1, text2, text3,text4,text5;
 
+  var text1, text2, text3, text4, text5;
+  late var rateczk, ratepln, raterub, ratekzt;
   @override
   void initState() {
     super.initState();
     futureAlbum = fetchAlbum();
+    rateczk =
+        getraterub()["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
   }
 
   @override
@@ -65,72 +75,98 @@ class _MyAppState extends State {
           style: Theme.of(context).textTheme.headline2!,
           textAlign: TextAlign.center,
           child: Center(
-            child: FutureBuilder<Album>(
-              future: futureAlbum,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  if (snapshot.hasData) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("PLN: ${snapshot.data!.pln}"),
-                         TextField(
-                          controller: controller1,
-                          decoration: const InputDecoration(
-                            hintText: 'Enter PLN Value',
-                          ),
-                        ),
-                        Text("EUR: ${snapshot.data!.eur}"),
-                         TextField(
-                          controller: controller2,
-                          decoration: const InputDecoration(
-                            hintText: 'Enter EUR Value',
-                          ),
-                        ),Text("RUB: ${snapshot.data!.rub}"),
-                         TextField(
-                          controller: controller3,
-                          decoration: const InputDecoration(
-                            hintText: 'Enter RUB Value',
-                          ),
-                        ),Text("CZK: ${snapshot.data!.czk}"),
-                         TextField(
-                          controller: controller4,
-                          decoration: const InputDecoration(
-                            hintText: 'Enter CZK Value',
-                          ),
-                        ),Text("KZT: ${snapshot.data!.kzt}"),
-                        TextField(
-                          controller: controller5,
-                          decoration: const InputDecoration(
-                            hintText: 'Enter KZT Value',
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            if(controller1.text=='') {text1=snapshot.data!.pln;} else {text1=double.parse(controller1.text);}                              
-                            if(controller2.text=='') {text2=snapshot.data!.eur;} else {text2=double.parse(controller2.text);}
-                            if(controller3.text=='') {text3=snapshot.data!.rub;} else {text3=double.parse(controller3.text);}
-                            if(controller4.text=='') {text4=snapshot.data!.czk;} else {text4=double.parse(controller4.text);}
-                            if(controller5.text=='') {text5=snapshot.data!.kzt;} else {text5=double.parse(controller5.text);}                              
-              setState(() {
-                              futureAlbum =
-                                  updateAlbum(text1,text2,text3,text4,text5);
-                            });
-                          },
-                          child: const Text('Update Data'),
-                        ),
-                      ],
-                    );
-                  } else if (snapshot.hasError) {
-                    return Text('${snapshot.error}');
-                  } else {
-                    return const CircularProgressIndicator();
-                  }
-                }
-                else  {
-                    return const CircularProgressIndicator();
-                  }
-              },
+            child: Column(
+              children: [
+                FutureBuilder<Album>(
+                  future: futureAlbum,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("PLN: ${snapshot.data!.pln}"),
+                            TextField(
+                              controller: controller1,
+                              decoration: const InputDecoration(
+                                hintText: 'Enter PLN Value',
+                              ),
+                            ),
+                            Text("EUR: ${snapshot.data!.eur}"),
+                            TextField(
+                              controller: controller2,
+                              decoration: const InputDecoration(
+                                hintText: 'Enter EUR Value',
+                              ),
+                            ),
+                            Text("RUB: ${snapshot.data!.rub}"),
+                            TextField(
+                              controller: controller3,
+                              decoration: const InputDecoration(
+                                hintText: 'Enter RUB Value',
+                              ),
+                            ),
+                            Text("CZK: ${snapshot.data!.czk}"),
+                            TextField(
+                              controller: controller4,
+                              decoration: const InputDecoration(
+                                hintText: 'Enter CZK Value',
+                              ),
+                            ),
+                            Text("KZT: ${snapshot.data!.kzt}"),
+                            TextField(
+                              controller: controller5,
+                              decoration: const InputDecoration(
+                                hintText: 'Enter KZT Value',
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                if (controller1.text == '') {
+                                  text1 = snapshot.data!.pln;
+                                } else {
+                                  text1 = double.parse(controller1.text);
+                                }
+                                if (controller2.text == '') {
+                                  text2 = snapshot.data!.eur;
+                                } else {
+                                  text2 = double.parse(controller2.text);
+                                }
+                                if (controller3.text == '') {
+                                  text3 = snapshot.data!.rub;
+                                } else {
+                                  text3 = double.parse(controller3.text);
+                                }
+                                if (controller4.text == '') {
+                                  text4 = snapshot.data!.czk;
+                                } else {
+                                  text4 = double.parse(controller4.text);
+                                }
+                                if (controller5.text == '') {
+                                  text5 = snapshot.data!.kzt;
+                                } else {
+                                  text5 = double.parse(controller5.text);
+                                }
+                                setState(() {
+                                  futureAlbum = updateAlbum(
+                                      text1, text2, text3, text4, text5);
+                                });
+                              },
+                              child: const Text('Update Data'),
+                            ),
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text('${snapshot.error}');
+                      } else {
+                        return const CircularProgressIndicator();
+                      }
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                ),
+              ],
             ),
           ),
         ),
